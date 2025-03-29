fd768d978c7a88c61a51688cdd78f0869a7d4dab
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-03-29 16:34:52 -0600

    Add Calli asset, colors, models for answers and views for quesitons.

---
+++ MVP/Preview Content/Views/AnswerButtonView.swift
@@ -0,0 +1,33 @@
+//
+//  AnswerButtonView.swift
+//  MVP
+//
+//  Created by Leo A.Molina on 29/03/25.
+//
+
+import SwiftUI
+
+struct AnswerButtonView: View {
+    @State public var answer: Answer
+    @State public var isSelected: Bool
+    var body: some View {
+        VStack{
+            Text(answer.answerText)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .foregroundColor(isSelected ? .white : .black)
+                .bold(isSelected)
+                .background(isSelected ? (answer.correct ? .green : .red) : .white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(isSelected ? (answer.correct ? .green : .red) : mainColor, lineWidth: 2)
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+        }
+    }
+}
+
+#Preview {
+    AnswerButtonView(answer: Answer(answerText: "Option", correct: false), isSelected: false)
+}
