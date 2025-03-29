cada49ce3fc0dcdaa9d2b6f4444a62b7c7c39e93
Daniela Caiceros <caiceros.flores.daniela@gmail.com>
2025-03-29 08:51:20 -0600

    Initial commit

---
+++ MVP/MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by Daniela Caiceros on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
